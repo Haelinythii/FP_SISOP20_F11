dfcf956537fcf208f15551d8fbe1abb64fb7a445
Haelinythii <47884588+Haelinythii@users.noreply.github.com>
2020-05-08 17:15:48 +0700

    ganti touch

--- touch.c
+++ touch.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char *argv[])
 {
-    int fd;
+    int fd, fd1;
 	if(argc<2){
 		printf(1,"Need at least 1 argument!\n");
 		exit();
@@ -15,18 +15,24 @@ int main(int argc, char *argv[])
     {
         for (int i = 1; i < argc; i++)
         {
-            if( (fd = open(argv[i], O_CREATE|O_RDWR)) < 0)
+            char isiLama[1024];
+            if((fd = open(argv[i], O_RDONLY)) > -1)
+            {
+                read(fd, isiLama, sizeof(isiLama));
+                // printf(1, "text: %s\n", isiLama);
+                unlink(argv[i]);
+                close(fd);
+            }
+
+            if( (fd1 = open(argv[i], O_CREATE|O_RDWR)) < 0)
             {
                 printf(1, "touch: can't create %s\n", argv[i]);
             }
-            printf(1, "created %s\n", argv[i]);
-            close(fd);
-        }
-        
-        
-    }
-    
+            else printf(1, "created %s\n", argv[i]);
 
-	
+            write(fd1, isiLama, strlen(isiLama));
+            close(fd1);
+        }
+    }
 	exit();
 }
